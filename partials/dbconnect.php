faff50aabe84616d45ced09f2f3525ade0525d5b
vishnu <vishnu@yeshnasolutions.com>
2025-02-03 11:28:19 +0530

    new

--- partials/dbconnect.php
+++ partials/dbconnect.php
@@ -1,15 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = ""; // Change this if you have set a password for your database
-$dbname = "projectx";
-$port = 3306; // Change this if your MySQL server uses a different port
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $db="projectx";
+  $conn = mysqli_connect($servername, $username, $password, $db);
+if($conn){
+ //code for connection success 
+}
+else
+{
+    die("Error" . mysqli_connect_error());
 }
 ?>
